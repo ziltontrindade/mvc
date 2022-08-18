eadb1e7a35e8a105060947b4d21f062b020df711
Zilton Cordeiro <ztc941@gmail.com>
2022-08-18 13:34:06 -0300

    View Dinamica

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -6,7 +6,17 @@ use \core\Controller;
 class HomeController extends Controller {
 
     public function index() {
-        $this->render('home', ['nome' => 'Zilton']);
+        $posts = [
+            ['titulo' => 'Titulo de Teste 1', 'corpo' => 'Corpo de teste 1' ],
+            ['titulo' => 'Titulo de Teste 2', 'corpo' => 'Corpo de teste 2' ],
+            ['titulo' => 'Titulo de Teste 3', 'corpo' => 'Corpo de teste 3' ],
+            ['titulo' => 'Titulo de Teste 4', 'corpo' => 'Corpo de teste 4' ],
+            ['titulo' => 'Titulo de Teste 5', 'corpo' => 'Corpo de teste 5' ],
+        ];
+
+        $this->render('home', [
+            'posts' => $posts
+        ]);
     }
 
     public function sobre() {
--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,3 +1,7 @@
 <?php $render('header'); ?>
 
-Opa, <?=$nome;?>
+<?php foreach($posts as $post) : ?>
+
+<h3><?php echo $post['titulo']; ?></h3>
+<p><?php echo $post['corpo']; ?></p>
+<?php endforeach; ?>
